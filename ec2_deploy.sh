25bcc73499fb9061b2e2517660099f2aac9b2b6b
Itao-o-bong✨ <45539206+itaobong@users.noreply.github.com>
2024-03-29 14:53:36 +0100

    Create ec2_deploy.sh

---
+++ ec2_deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Script Name: setup_web_server.sh
+# Description: This script automates the setup of a basic web server environment with Apache HTTP Server.
+# AMI: Use Amazon Linux  
+
+# The script is intended to be used on an Amazon EC2 instance running Amazon Linux, 
+
+# Update system packages
+sudo yum update -y
+# The '-y' flag automatically answers yes to any prompts during the update process.
+
+# Install Apache HTTP Server
+sudo yum install -y httpd
+
+# Start Apache HTTP Server
+sudo systemctl start httpd
+
+# Enable Apache HTTP Server to start on boot
+sudo systemctl enable httpd
+
+# Download and unzip HTML app
+# Download the HTML app ZIP file
+wget https://mequestorg.s3.us-west-2.amazonaws.com/e-commerce.zip
+# Use wget to download the ZIP file from the specified URL.
+
+# Unzip the downloaded ZIP file
+unzip e-commerce.zip
+# Unzip the downloaded ZIP file to extract its contents.
+
+# Move HTML app to the web server's document root
+# Move the contents of the extracted directory to the document root of Apache
+sudo mv e-commerce/* /var/www/html/
+# Move all files and directories inside the 'profile-main' directory to '/var/www/html/',
+# which is the default document root for Apache HTTP Server.
